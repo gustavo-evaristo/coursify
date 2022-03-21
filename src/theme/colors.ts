6b835947603207ba00ca5c52bccefaac3d77897d
Gustavo Evaristo <gug.henri1@gmail.com>
2022-03-21 13:38:25 -0300

    feat: theme

---
+++ src/theme/colors.ts
@@ -0,0 +1,23 @@
+export const colors = {
+  green: {
+    primary: '#3CC6AA',
+    secondary: '#1ABC9C',
+    tertiary: '#2AB598',
+  },
+
+  yellow: {
+    primary: '#FDA506',
+    secondary: '#FFA900',
+  },
+
+  gray: {
+    primary: '#00000029',
+    secondary: '#868686',
+    tertiary: '#535353',
+  },
+
+  white: {
+    primary: '#FFFFFF',
+    secondary: '#0000000F',
+  },
+};
