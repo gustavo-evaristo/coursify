3bf46733848451273076787a8cbfc4d92bfaab2b
Gustavo Evaristo <gug.henri1@gmail.com>
2022-03-21 15:53:04 -0300

    feat: header

---
+++ custom.d.ts
@@ -0,0 +1,4 @@
+declare module '*.png' {
+  const contentPng: any;
+  export default contentPng;
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,12 @@
 import React from 'react';
-import {Container} from './styles';
+import {Container, MenuContent, Image, MenuItem} from './styles';
+import Logo from '../../assets/images/logo.png';
 
-export const Header: React.FC = ({...rest}) => <Container {...rest} />;
+export const Header: React.FC = ({...rest}) => (
+  <Container {...rest}>
+    <Image source={Logo} />
+    <MenuContent>
+      <MenuItem />
+    </MenuContent>
+  </Container>
+);
