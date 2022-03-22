ffb63a4935c8fec7796560b5b3795a5ba66bb584
Gustavo Evaristo <gug.henri1@gmail.com>
2022-03-22 12:06:43 -0300

    fix: helper

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
-export const textReplace = (text: string): string =>
+export const textReplace = (text?: string): string =>
   text
-    .replace(/<[^>]+>/g, '')
+    ?.replace(/<[^>]+>/g, '')
     .replace(/&#8230;/g, '')
     .replace(/&#8217;/g, '');
