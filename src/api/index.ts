0382c7578bfa00bfd1f5231723ee66e58f2eae13
Gustavo Evaristo <gug.henri1@gmail.com>
2022-03-21 22:40:14 -0300

    feat: api

---
+++ src/api/index.ts
@@ -0,0 +1,50 @@
+import axios, {AxiosInstance} from 'axios';
+
+class Api {
+  private request: AxiosInstance;
+
+  constructor() {
+    this.request = axios.create({
+      baseURL: 'https://blog.coursify.me/wp-json/wp/v2',
+      timeout: 30000,
+    });
+  }
+
+  public async getPosts(): Promise<PostType[]> {
+    try {
+      const {data} = await this.request.get('/posts');
+      return data;
+    } catch (err) {
+      return [];
+    }
+  }
+
+  public async getPost(id: number): Promise<PostType> {
+    try {
+      const {data} = await this.request.get(`/posts/${id}`);
+      return data;
+    } catch (err) {
+      return null;
+    }
+  }
+
+  public async getCategories(): Promise<CategoryType[]> {
+    try {
+      const {data} = await this.request.get('/categories');
+      return data;
+    } catch (err) {
+      return [];
+    }
+  }
+
+  public async getMedia(id: number): Promise<MediaType> {
+    try {
+      const {data} = await this.request.get(`/media/${id}`);
+      return data;
+    } catch (err) {
+      return null;
+    }
+  }
+}
+
+export default new Api();
