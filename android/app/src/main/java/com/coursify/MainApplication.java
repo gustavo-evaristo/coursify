85d281bd811f29631bd8b4bafe88e601589b750f
Gustavo Evaristo <gug.henri1@gmail.com>
2022-03-23 01:30:19 -0300

    build

--- android/app/src/main/java/com/coursify/MainApplication.java
+++ android/app/src/main/java/com/coursify/MainApplication.java
@@ -5,8 +5,6 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.reactnativecommunity.picker.RNCPickerPackage;
-import com.BV.LinearGradient.LinearGradientPackage;
-import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
